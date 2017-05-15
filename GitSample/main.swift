a3f59172886d00a329ef3521eeb07b52c3f1139b
Tada <tada.csc@gmail.com>
2017-05-15 17:02:18 +0900

    hello world!

--- GitSample/main.swift
+++ GitSample/main.swift
@@ -8,23 +8,4 @@
 
 import Foundation
 
-
-func favoritePlace(str1:String) {
-    print("I like " + str1 + ".")
-}
-
-func showEncounterMessage(monster: String, count : Int) {
-    print("\(count)匹の\(monster)が現れた！");
-}
-
-func hogehoge(foo : String)
-{
-    print("hogee");
-}
-
-print("Hello, World!")
 print("こんにちは世界！！！")
-
-favoritePlace(str1: "nagasaki")
-
-showEncounterMessage(monster: "スライム", count: 3)
